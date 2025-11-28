347368dbb02332a4f6492b67291eae77b6c0e575
이정환 <king199777@gmail.com>
2025-11-28 17:05:58 +0900

    예제코드 1차 업로드

---
+++ 11.chapter09/src/component/TodoItem.jsx
@@ -0,0 +1,36 @@
+import { memo, useContext } from "react";
+import "./TodoItem.css";
+import { TodoDispatchContext } from "../App";
+
+const TodoItem = ({ id, content, isDone, createdDate }) => {
+  console.log(`${id} TodoItem 업데이트`);
+  const { onUpdate, onDelete } = useContext(TodoDispatchContext);
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <div className="checkbox_col">
+        <input
+          checked={isDone}
+          type="checkbox"
+          onChange={onChangeCheckbox}
+        />
+      </div>
+      <div className="title_col">{content}</div>
+      <div className="date_col">
+        {new Date(createdDate).toLocaleDateString()}
+      </div>
+      <div className="btn_col">
+        <button onClick={onClickDelete}>삭제</button>
+      </div>
+    </div>
+  );
+};
+export default memo(TodoItem);
